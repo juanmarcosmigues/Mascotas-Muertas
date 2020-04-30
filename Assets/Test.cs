92822ef0770e363916c6bb21c7d60fcb61ba8dff
Juan Marcos Migues <juanmarcosmigues@gmail.com>
2020-04-30 12:14:34 -0300

    lala

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            meshRenderer.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        transform.Rotate(Vector3.up * Time.deltaTime * 10f);
+    }
+}
